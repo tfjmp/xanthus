51dc70c7416119f1a2f8bdc15dcc9a01660548d4
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2019-03-04 14:41:52 +0000

    should be working, need new spade VM to finish testing

--- lib/pegasus/default.rb
+++ lib/pegasus/default.rb
@@ -1,10 +1,21 @@
 module Pegasus
 CAMFLOW_START = %q{%{
 camflow -a true
+sleep 1
 }}
 
 CAMFLOW_STOP = %q{%{
 camflow -a false
+sleep 20
+}}
+
+SPADE_START = %q{%{
+echo spade | sudo -H -u spade ../SPADE/bin/spade start
+sleep 1
+}}
+
+SPADE_STOP = %q{%{
+echo spade | sudo -H -u spade ../SPADE/bin/spade stop
 sleep 20
 }}
 end
