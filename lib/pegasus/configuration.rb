0ba45278e4bb839aefdc2285ca4d3f0fc2359648
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2019-03-04 10:34:35 +0000

    working job iterations

--- lib/pegasus/configuration.rb
+++ lib/pegasus/configuration.rb
@@ -33,42 +33,14 @@ module Pegasus
     end
   end
 
-  def self.write_template file, template, config
-    v = eval(template)
-    if v.is_a? Enumerable
-      v.each do |s|
-        file.write(s+"\n")
-      end
-    else
-      file.write(v+"\n")
-    end
-  end
-
-  def self.generate_script name, template, sets, config
-    FileUtils.mkdir_p name
-    Dir.chdir name do
-      for i in 0..sets do
-        File.open(name+'_'+i.to_s+'.sh', 'w+') do |f|
-            f.write("# pre-script\n")
-            self.write_template f, config.pre_script, config
-            f.write("\n")
-            f.write("# script\n")
-            self.write_template f, template, config
-            f.write("\n")
-            f.write("# post-script\n")
-            self.write_template f, config.post_script, config
-            f.write("\n")
-        end
-      end
-    end
-  end
-
   def self.configure
     config = Configuration.new
     yield(config)
     puts "Running experiment #{config.name} with seed #{config.seed}."
     config.jobs.each do |name,job|
-      job.execute config, 0
+      for i in 0..(job.iterations-1) do
+        job.execute config, i
+      end
     end
   end
 end
