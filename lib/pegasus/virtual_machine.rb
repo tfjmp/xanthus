d222504971b15c683ca523d98cd61cef3b3b6a90
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2019-03-05 09:09:37 +0000

    make most vagrant parameters configurable

--- lib/pegasus/virtual_machine.rb
+++ lib/pegasus/virtual_machine.rb
@@ -3,13 +3,21 @@ module Pegasus
     attr_accessor :name
     attr_accessor :box
     attr_accessor :version
+    attr_accessor :cpus
+    attr_accessor :cpu_cap
+    attr_accessor :memory
     attr_accessor :ip
+    attr_accessor :gui
 
     def initialize
       @name = :default
       @box = 'jhcook/fedora27'
       @version = '4.13.12.300'
       @ip = '192.168.33.8'
+      @memory = 4096
+      @cpus = 2
+      @cpu_cap = 70
+      @gui = false
     end
 
     def to_vagrant
@@ -20,10 +28,10 @@ Vagrant.configure(2) do |config|
   config.vm.network "private_network", ip: "#{@ip}"
 
   config.vm.provider "virtualbox" do |vb|
-   vb.gui = false
-   vb.memory = 4096
-   vb.customize ["modifyvm", :id, "--cpuexecutioncap", "70"]
-   vb.cpus = 2
+   vb.gui = #{@gui}
+   vb.memory = #{@memory}
+   vb.customize ["modifyvm", :id, "--cpuexecutioncap", "#{@cpu_cap}"]
+   vb.cpus = #{@cpus}
    vb.name = "#{@name}"
   end
   config.vm.provision "shell", path: "provision.sh"
